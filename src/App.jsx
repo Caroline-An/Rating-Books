c1ced2a371c12bfe35a47b76439d2a6554c17f8e
Caroline-An <anacarolinesn@outlook.com>
2025-05-06 22:11:57 -0300

    adicionando App.jsx e css, removendo counter.ts, atualizando main de ts para jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Rating Books</h1>
+      <p>App de classificação de livros</p>
+    </div>
+  );
+}
+
+export default App;
